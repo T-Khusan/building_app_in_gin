02e14fb3435001399e756ca2bb393f3480904e9f
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2022-04-15 15:14:13 +0500

    Added custom handler

--- chapter01/hello-world/main.go
+++ chapter01/hello-world/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IndexHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "hello world",
+	})
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
-	router.Run()
+	router.GET("/", IndexHandler)
+	http.ListenAndServe(":8080", router)
 }
